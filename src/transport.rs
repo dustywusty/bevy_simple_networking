eb515fcd4eec88851f20ec0d7e359f393489ac4f
Dusty <dustyc@me.com>
2022-11-28 02:44:50 -0600

    Update transport.rs

--- src/transport.rs
+++ src/transport.rs
@@ -1,10 +1,11 @@
+use bevy_ecs::system::Resource;
 use std::{collections::VecDeque, net::SocketAddr};
 
 use super::message::Message;
 
 /// Resource serving as the owner of the queue of messages to be sent. This resource also serves
 /// as the interface for other systems to send messages.
-#[derive(Debug, Resource)]
+#[derive(Resource)]
 pub struct Transport {
     messages: VecDeque<Message>,
 }
